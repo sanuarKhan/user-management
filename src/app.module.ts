b327053ed321b19b74a0edf3f27ba41af30870c9
sanuarkhan220 <sanuarkhan220@gmail.com>
2026-06-12 16:13:38 +0600

    feat: implement user management features with bcrypt integration for password hashing

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [],
   providers: [PrismaService],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.usersService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() data: Partial<CreateUserDto>) {
+    return this.usersService.update(id, data);
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+}
