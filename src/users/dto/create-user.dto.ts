e560291c37b22833cb254d852a255edef881ca29
sanuarkhan220 <sanuarkhan220@gmail.com>
2026-06-12 10:41:37 +0600

    nestjs project up

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  profileImage?: string;
+}
